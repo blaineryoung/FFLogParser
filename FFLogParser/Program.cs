875e3217ff68356676dbcd53190a36be89c699e8
Blaine Young <blaineryoung@hotmail.com>
2022-01-02 21:36:11 -0800

    Get healing events

--- FFLogParser/Program.cs
+++ FFLogParser/Program.cs
@@ -33,29 +33,54 @@ namespace FatedLogParser // Note: actual namespace depends on the project name.
                 p = playerMappings;
             }
 
-            EventDatum[] events = await ffLogClient.GetEvents(reportId, t);
+            Tuple<int, int> currentTimestamp = new Tuple<int, int>(t.Item1, t.Item2);
+            int daktId = p.Where(x => x.Value == "Dakt Cole").FirstOrDefault().Key;
 
-            Console.WriteLine("Time, Ability, Source, Target, Ammount, Type");
-
-            foreach(EventDatum e in events)
+            Console.WriteLine("Time, Source, Ability, Target, Ammount, Type, Overheal, Tick");
+            for (int i = 0; i < 10; i++) // limit to 10 for testing
             {
-                int time = e.timestamp - t.Item1;
-                string abilityName = await ffLogClient.GetAbilityName(e.abilityGameID);
+                int highTime = currentTimestamp.Item1;
+                EventDatum[] events = await ffLogClient.GetEvents(reportId, currentTimestamp);
 
-                string sourceName;
-                if (false == p.TryGetValue(e.sourceID, out sourceName))
+                if (events.Length == 0)
                 {
-                    sourceName = "Boss";
+                    break;
                 }
 
-                string targetName;
-                if (false == p.TryGetValue(e.sourceID, out targetName))
+                highTime = events[events.Length - 1].timestamp;
+
+                IEnumerable<EventDatum> filteredEvents = events.Where(x =>
+                    ((x.type == "heal") && (x.targetID == daktId)) || // healing to dakt
+                    ((!p.ContainsKey(x.sourceID)) && (x.type == "calculateddamage") && (x.targetID == daktId)) // boss damage );
+                    );
+
+
+                foreach (EventDatum e in filteredEvents)
                 {
-                    targetName = "Boss";
+                    int time = (e.timestamp - t.Item1) /1000;
+                    string abilityName = await ffLogClient.GetAbilityName(e.abilityGameID);
+
+                    if (abilityName == "Unknown")
+                    {
+                        continue;
+                    }
+
+                    string sourceName;
+                    if (false == p.TryGetValue(e.sourceID, out sourceName))
+                    {
+                        sourceName = e.type == "heal" ? "Dakt Cole" : "Boss";
+                    }
+
+                    string targetName;
+                    if (false == p.TryGetValue(e.targetID, out targetName))
+                    {
+                        targetName = e.type == "heal" ? "Dakt Cole" : "Boss";
+                    }
+
+                    Console.WriteLine($"{time}, {sourceName}, {abilityName},  {targetName}, {e.amount}, {e.type}, {e.overheal}, {e.tick}");
                 }
 
-                Console.WriteLine($"{time}, {abilityName}, {sourceName}, {targetName}, {e.amount}, {e.type}");
-
+                currentTimestamp = new Tuple<int, int>(highTime, t.Item2);
             }
         }
     }
